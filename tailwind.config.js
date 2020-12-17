af692b4bfa854ec33671a1c00c3ecdbe6cfcdb4f
John Kelly <chippyfork@gmail.com>
2020-12-17 08:32:35 +0000

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    purge: {
+        mode: 'layers',
+        content: ['./public/**/*.html'],
+    },
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
